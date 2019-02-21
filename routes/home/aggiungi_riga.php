e4703c0ea60bbb08977485b0832ab683dcdc5d3b
Patrick <poup2000@gmail.com>
2019-02-21 12:18:19 +0100

    aaa

--- routes/home/aggiungi_riga.php
+++ routes/home/aggiungi_riga.php
@@ -5,5 +5,4 @@ $db = new PDO ("mysql:host=192.168.245.1;dbname=dbcrud", 'root', '');
 $stmt = $db->prepare('INSERT INTO tblpersone(Nome, Cognome, DataNascita, Sesso, Reddito) VALUES(?,?,?,?,?);');
 
 $stmt->execute([$_POST['nome'],$_POST['cognome'],$_POST['dataNascita'],$_POST['sesso'],$_POST['reddito']]);
-
 ?>
