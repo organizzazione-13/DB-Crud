1b37682bd3e585d1eb03bc4daf53d99d75b53462
Patrick <poup2000@gmail.com>
2019-02-22 14:50:35 +0100

    RAGNAROK

--- routes/home/elimina_riga.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbh = new PDO ("mysql:host=192.168.245.1;dbname=dbcrud", 'root', '');
-
-$sth = $dbh->prepare('DELETE FROM tblpersone WHERE Idpersona = ?');
-$sth->execute([$_POST['id']]);
-
-header("location: index.php?".$_POST['params']);
--- routes/home/modifica_riga.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbh = new PDO ("mysql:host=192.168.245.1;dbname=dbcrud", 'root', '');
-
-$sth = $dbh->prepare('UPDATE tblpersone SET Nome=?, Cognome=?, DataNascita=?, Sesso=?, Reddito=? WHERE Idpersona = ? ;');
-
-$sth->execute([$_POST['nome'],$_POST['cognome'],date_format(date_create_from_format('Y-m-d', $_POST['dataNascita']), 'Ymd'),$_POST['sesso'],$_POST['reddito'], $_POST['scopo']]);
-
-header("location: index.php?".$_POST['params']);
-?>
